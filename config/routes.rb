c0747b9c5e3ce8775c5d455b228a57f8546f3e95
黒瀬友太 <yuta.saijyo12@gmail.com>
2019-11-19 20:41:41 +0900

    Customized registration, login, and lgout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    get 'logout' => 'devise/sessions#destroy'
+  end
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   resources :portfolios, except: [:show]
   get "angular-items", to: "portfolios#angular"
   get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
